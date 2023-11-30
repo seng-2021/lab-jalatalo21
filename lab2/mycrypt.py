c468f92173275723aa4d5a458afbb660e85c1884
Juuso Alatalo <juuso.alatalo@student.oulu.fi>
2023-11-30 15:41:31 +0200

    commit 1

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,7 +8,15 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    
+    #maksimi
+    s = s.ljust(1000, "a")
+
     for c in s:
+        #virheelliset arvot
+        if c in ["å", "ä", "ö"]:
+            raise ValueError
+        
         if c.isalpha():
             if c.islower():
                 c=c.upper()
@@ -16,9 +24,11 @@ def encode(s):
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
+        #jos ei ole digit tai kirjain niin virhe
+        else:
+            raise ValueError
+    #alkuperäinen pituus
+    return crypted[:origlen]
 
 def decode(s):
-    return s
-
+    return encode(s).lower()
--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -42,12 +42,17 @@ def test_invalid_char(invalid_input):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [1, 1.1])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
         mycrypt.encode(invalid_input)
 
+@pytest.mark.parametrize("invalid_input", ["1"*2000, "2"*3000])
+def test_invalid_lenght(invalid_input):
+    '''Invalid input parameter lenght should raise ValueError'''
+    with pytest.raises(ValueError):
+        mycrypt.encode(invalid_input)
 
 def test_timing():
     '''Test whether encoding runs in approximately constant time, repetitions
